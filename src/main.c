5cee859e0d2e6197635bc2a2518f93d9ac33b661
step- <step-@users.noreply.github.com>
2025-12-02 10:55:57 +0100

    fix: YAD_XID type [backport]
    Backported:
    - 2020-10-21 12eaed3 improve settings of YAD_XID

--- src/main.c
+++ src/main.c
@@ -1038,7 +1038,7 @@ main (gint argc, gchar ** argv)
       if (is_x11)
         {
           /* add YAD_XID variable */
-          str = g_strdup_printf ("0x%X", (guint) GDK_WINDOW_XID (gtk_widget_get_window (dialog)));
+          str = g_strdup_printf ("0x%lX", GDK_WINDOW_XID (gtk_widget_get_window (dialog)));
           g_setenv ("YAD_XID", str, TRUE);
         }
 
